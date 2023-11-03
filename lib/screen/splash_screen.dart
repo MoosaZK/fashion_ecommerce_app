2bda9ae2bbc55d73898100f6d281b613bcae2a35
MoosaZk <moosazkhan@gmail.com>
2023-11-03 21:47:55 +0500

    added animation to splashcreen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -11,18 +11,19 @@ class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
-
+  late Animation<Offset> _offsetAnimation;
   @override
   void initState() {
     super.initState();
 
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2), // Adjust the duration as needed
+      duration: const Duration(seconds: 2), // Adjust the duration as needed
     );
 
     _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
-
+    _offsetAnimation = Tween(begin: const Offset(-0.2, 0), end: Offset.zero)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
     _controller.forward();
 
     _animation.addListener(() {
@@ -33,54 +34,54 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Opacity(
-              opacity: _animation.value, // Use the animation value for opacity
-              child: Text(
-                "V",
-                style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+      body: Center(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FadeTransition(
+            opacity: _animation,
+            child: Container(
+              height: 100,
+              width: 100,
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: Colors.brown,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const Center(
+                child: Text(
+                  "F",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "THE",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                  margin: EdgeInsets.symmetric(horizontal: 10),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "VOGUE",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(
-                  "CULT ",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+          ),
+          // ),
+          const SizedBox(
+            width: 10,
+          ),
+          SlideTransition(
+            position: _offsetAnimation,
+            child: FadeTransition(
+              opacity: _animation,
+              child: const Text(
+                "fashion",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 
   @override
   void dispose() {
     _controller.dispose();
+
     super.dispose();
   }
 }
